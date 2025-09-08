81ec3d1d6bdd45fee2abc19e90ee295ac8f308be
Arjun <jaluarjun442@gmail.com>
2025-09-08 23:35:31 +0530

    customer migration created

---
+++ database/migrations/2025_09_08_150141_create_customer_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateCustomerTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('customer', function (Blueprint $table) {
+            $table->id();
+            $table->string('name')->nullable();
+            $table->string('mobile')->nullable();
+            $table->string('email')->nullable();
+            $table->longText('address')->nullable();
+            $table->string('gst')->nullable();
+            $table->string('pan')->nullable();
+            $table->boolean('is_active')->comment('1-active,0-inactive')->default(1);
+            $table->softDeletes();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('customer');
+    }
+}
